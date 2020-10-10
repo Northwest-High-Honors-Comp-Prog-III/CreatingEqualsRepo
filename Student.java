3df6be84d0a00cc8b767f1f418d99757cb4579c0
sellstep <stephen_p_sell@mcpsmd.org>
2020-10-10 16:48:51 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,18 @@
+/*
+What goes here?
+*/
+
+
+public class Student extends Person {
+
+    private int grade;
+
+    public Student(String name, String birthday, int grade){
+        super(name, birthday);
+        this.grade = grade;
+    }
+
+    public int getGrade(){
+        return grade;
+    }
+}
